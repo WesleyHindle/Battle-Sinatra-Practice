eabed7d59dfbdd32770684f4e24f9616ea26cf9d
Wesley Hindle <wesleyhindle@gmail.com>
2021-08-04 17:03:58 +0100

    Added a model layer and extracted logic to it.

--- app.rb
+++ app.rb
@@ -13,21 +13,20 @@ class Battle < Sinatra::Base
   end 
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name] #session[:var_name] allows vars to be stored in session instead of instance vars
-    session[:player_2_name] = params[:player_2_name]
-    p params
+    $player_1 = Player.new(params[:player_1_name]) #session[:var_name] allows vars to be stored in session instead of instance vars
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do 
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end 
 
   get '/attack' do 
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
   end 
 
