ad8455b9bf2692ba3e28a8bc35061a6700c4284a
Wesley Hindle <wesleyhindle@gmail.com>
2021-08-04 16:15:20 +0100

    Refactored code to have a helper file. Added this helper to the spec_helper.

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,12 +1,7 @@
 feature 'Enter names' do 
   scenario 'submitting names' do 
-    visit('/')
-    fill_in :player_1_name, with: 'Anabelle'
-    fill_in :player_2_name, with: 'Beatrice'
-    click_button 'Submit'
-
+    sign_in_and_play #It is specified in the spec_helper where to look for this method
     save_and_open_page #Saves web page and opens the browser to display it
-
     expect(page).to have_content 'Anabelle vs. Beatrice'
   end
 end 
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,10 +1,6 @@
 feature "View hit points" do 
   scenario "See player 2's hit points" do
-    visit('/')
-    fill_in :player_1_name, with: "John"
-    fill_in :player_2_name, with: "Doe"
-    click_button 'Submit'
-    expect(page).to have_content "Doe: 60HP"
-
+    sign_in_and_play
+    expect(page).to have_content "Beatrice: 60HP"
   end
 end 
